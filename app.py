f8bc4a02582967017c06f8b501d3138a3ede9030
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-03-15 17:31:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,16 @@
+
 from flask import Flask
 from flask_cors import CORS
-import scholarly.scholarly
+
+from controllers import ApiRoutes, ErrorRoutes, FakeApiRoutes
 
 app = Flask(__name__)
 cors = CORS(app)
 
-
-@app.route('/authors/<author_name>', methods=['GET'])
-def get_author(author_name):
-    
-    print("stage one :")
-    print(author_name)
-
-    search_query = scholarly.search_author(author_name)
-    print("stage two :")
-    print(search_query)
-
-    author = next(search_query).fill()
-    print("stage three :")
-    print(author)
-
-    return author.toJSON()
-
-@app.route('/test/authors/<author_name>', methods=['GET'])
-def get_test_author(author_name):
-    f = open("json.txt", "r")
-    return f.read()
-
+# Blueprint register
+app.register_blueprint(ApiRoutes.bp)
+app.register_blueprint(FakeApiRoutes.bp,url_prefix='/fake-api')
+app.register_blueprint(ErrorRoutes.bp)
 
 if __name__ == '__main__':
     app.run()
