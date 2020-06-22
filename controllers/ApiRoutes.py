340f3ed5a428c8840a79c93f72e78620fb510df7
Akram Aznakour <35656299+AkramAznakour@users.noreply.github.com>
2020-06-22 20:05:08 +0100

    Update ApiRoutes.py

--- controllers/ApiRoutes.py
+++ controllers/ApiRoutes.py
@@ -8,12 +8,14 @@ bp = Blueprint('api', __name__)
 @bp.route('/authors/<author_name>', methods=['GET'])
 def get_author(author_name):
     try:
+        print("author_name : ",author_name)
         # Search by author name and return a generator of Author objects
         search_query = scholarly.search_author(author_name)
         # Populate the Author with information from their profile
         author = next(search_query).fill()
 
     except Exception as exception:
+        print("exception")
         return jsonify(sucess=False,
                        message='author was not found',
                        status=HTTPStatus.NOT_FOUND.value,
@@ -27,6 +29,7 @@ def get_author(author_name):
             file.write(author.toJSON())
 
     except Exception as exception:
+        print("exception")
         return jsonify(sucess=False,
                        message=str(exception),
                        status=HTTPStatus.INTERNAL_SERVER_ERROR.value,
